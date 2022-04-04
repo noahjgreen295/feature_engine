a9e14621a3991ea7f3ff34fd40e343bd763a7ec1
Soledad Galli <solegalli@protonmail.com>
2022-04-04 16:41:26 +0200

    changes wording adds typehint

--- feature_engine/numpy_to_pandas.py
+++ feature_engine/numpy_to_pandas.py
@@ -8,9 +8,7 @@ import pandas as pd
 
 def _is_numpy(obj_in: Any) -> bool:
     """
-    Tests if an object is a numpy object.
-    If the input is a numpy array, it converts it to a pandas Dataframe. This is mostly
-    so that we can add the check_estimator checks for compatibility with sklearn.
+    Checks if object is a numpy object.
 
     Parameters
     ----------
@@ -24,19 +22,20 @@ def _is_numpy(obj_in: Any) -> bool:
 
 
 def _numpy_to_dataframe(
-    obj_in: Union[np.generic, np.ndarray], index=None
+        obj_in: Union[np.generic, np.ndarray],
+        index: Union[np.generic, np.ndarray, pd.Series, None] = None
 ) -> pd.DataFrame:
     """
-    Converts a numpy object to a pandas DataFrame
+    Converts a numpy object to a pandas DataFrame.
 
     Parameters
     ----------
-    obj_in : the object to convert
-    index : array-like (optional); will set index on DataFrame
+    obj_in : the object to convert.
+    index : array-like (optional); will set index on DataFrame.
 
     Returns
     -------
-    df_out : the object converted to a pandas DataFrame
+    df_out : the object converted to a pandas DataFrame.
     """
     col_names: List[str] = [str(i) for i in range(obj_in.shape[1])]
     df_out: pd.DataFrame = pd.DataFrame(obj_in, columns=col_names, index=index)
@@ -44,14 +43,17 @@ def _numpy_to_dataframe(
     return df_out
 
 
-def _numpy_to_series(obj_in: Union[np.generic, np.ndarray], index=None) -> pd.Series:
+def _numpy_to_series(
+        obj_in: Union[np.generic, np.ndarray],
+        index: Union[np.generic, np.ndarray, pd.Series, None] = None,
+) -> pd.Series:
     """
-    Converts a numpy object to a pandas Series
+    Converts a numpy object to a pandas Series.
 
     Parameters
     ----------
-    obj_in : the object to convert
-    index : array-like (optional); will set index on Series
+    obj_in : the object to convert.
+    index : array-like (optional); will set index on Series.
 
     Returns
     -------
